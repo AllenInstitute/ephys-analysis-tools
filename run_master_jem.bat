d387497d94cfd9590a3dbb899fffeba7cfd1c760
Ram Rajanbabu <ramr@alleninstitute.org>
2021-10-07 15:55:40 -0700

    Create bat file to run jem_template.py.

---
+++ run_master_jem.bat
@@ -0,0 +1,3 @@
+call C:\Users\ramr\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
+call python C:\Users\ramr\Documents\Github\ai_repos\ephys_analysis_tools\src\jem_templates\python_files\jem_template.py
+@pause
