ae62b2d00c0ec42dafabe896ab2fbcbe0863382a
Ram Rajanbabu <ramr@alleninstitute.org>
2022-04-25 09:03:17 -0700

    Updated bat file to run recently updated generate_master_jem.py.

--- run_master_jem.bat
+++ run_master_jem.bat
@@ -1,3 +1,3 @@
 call C:\Users\ramr\Anaconda3\Scripts\activate.bat
-call python C:\Users\ramr\Documents\Github\ai_repos\ephys_analysis_tools\src\jem_templates\jem_template.py
+call python C:\Users\ramr\Documents\Github\ai_repos\ephys_analysis_tools\src\run_scripts\generate_master_jem.py
 @pause
