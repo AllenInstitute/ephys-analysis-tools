e98697dd360d8435a06bb633f68baeadf8a82d40
Ram Rajanbabu <ramr@alleninstitute.org>
2022-11-20 11:07:08 -0800

    Removed @pause from bat files.

--- run_generate_pipeline_metrics.bat
+++ run_generate_pipeline_metrics.bat
@@ -3,4 +3,3 @@ call %USERPROFILE%\Anaconda3\Scripts\activate.bat
 call activate ephys_analysis_tools_env
 call python src\run_scripts\generate_ephys_pipeline_metrics.py
 call conda deactivate
-@pause
--- run_ivscc_weekly_transcriptomics_report.bat
+++ run_ivscc_weekly_transcriptomics_report.bat
@@ -3,4 +3,3 @@ call %USERPROFILE%\Anaconda3\Scripts\activate.bat
 call activate ephys_analysis_tools_env
 call python src\run_scripts\ivscc_weekly_transcriptomics_report.py
 call conda deactivate
-@pause
