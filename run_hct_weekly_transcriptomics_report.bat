fee2a0014c28772a3b303cc59ce84581aefe664a
Ram Rajanbabu <ramr@alleninstitute.org>
2022-12-02 15:21:43 -0800

    Added @pause to bat files.

--- run_hct_weekly_transcriptomics_report.bat
+++ run_hct_weekly_transcriptomics_report.bat
@@ -3,3 +3,4 @@ call %USERPROFILE%\Anaconda3\Scripts\activate.bat
 call activate ephys_analysis_tools_env
 call python src\run_scripts\hct_weekly_transcriptomics_report.py
 call conda deactivate
+@pause
