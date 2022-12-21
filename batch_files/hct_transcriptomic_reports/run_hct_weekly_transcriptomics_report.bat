0bbcd6403ed9c3bb19785f41403f73fa4296f079
Ram Rajanbabu <ramr@alleninstitute.org>
2022-12-21 11:56:03 -0800

    Changed - to _ for folder names.

--- batch_files/hct_transcriptomic_reports/run_hct_weekly_transcriptomics_report.bat
+++ batch_files/hct_transcriptomic_reports/run_hct_weekly_transcriptomics_report.bat
@@ -2,6 +2,6 @@
 call %USERPROFILE%\Anaconda3\Scripts\activate.bat
 call cd..\..
 call activate ephys_analysis_tools_env
-call python src\run_scripts\hct_weekly_transcriptomics_report.py
+call python src\run-scripts\hct_weekly_transcriptomics_report.py
 call conda deactivate
 @pause
