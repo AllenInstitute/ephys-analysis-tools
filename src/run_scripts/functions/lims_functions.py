99a5c44ddf292bd03de703d49d6085ff0d99747c
Ram Rajanbabu <ramr@alleninstitute.org>
2023-02-20 11:21:53 -0800

    Added to user_codes and project_codes.

--- src/run_scripts/functions/lims_functions.py
+++ src/run_scripts/functions/lims_functions.py
@@ -111,8 +111,8 @@ def get_lims_dataframe(query):
 #-----Ram's functions----#
 def get_lims_ephys():
 
-    project_codes = ("hIVSCC-MET", "hIVSCC-METx", "hIVSCC-METc", "mIVSCC-MET", "mIVSCC-METx", "qIVSCC-METa", "mMPATCHx", "H301", "H301x")
-    user_codes = ("P1", "P2", "P4", "P8", "P9", "PA", "PB", "PC", "PE", "PJ", "PN", "PR", "PX")
+    project_codes = ("hIVSCC-MET", "hIVSCC-METx", "hIVSCC-METc", "mIVSCC-MET", "mIVSCC-METx", "qIVSCC-METa", "qIVSCC-METc", "mMPATCHx", "H301", "H301x")
+    user_codes = ("PC", "PX", "P1", "P2", "P4", "P6", "P8", "P9", "PA", "PB", "PE", "PJ", "PN", "PR", "PV")
 
     lims_query = """
     SELECT cell.name AS cell_name, 
