23526574e9c56e5b97834a6663e02337752bd154
kristenjh <kristenh@alleninstitute.org>
2023-09-08 12:58:24 -0700

    Added slice_level_project.

--- batch_files/ivscc_data_generation/run_generate_pipeline_metrics.bat
+++ batch_files/ivscc_data_generation/run_generate_pipeline_metrics.bat
@@ -1,5 +1,5 @@
 @rem %USERPROFILE% = C:\Users\%USERNAME%
-call %USERPROFILE%\Anaconda3\Scripts\activate.bat
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
 call cd..\..
 call activate ephys-analysis-tools-env
 call python src\run_scripts\generate_ephys_pipeline_metrics.py
