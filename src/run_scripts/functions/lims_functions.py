6d75839dc1ca317e425f0203a8350eeae1d252fe
kristenjh <kristenh@alleninstitute.org>
2024-06-12 08:29:57 -0700

    Added new project codes to lims_functions.

--- src/run_scripts/functions/lims_functions.py
+++ src/run_scripts/functions/lims_functions.py
@@ -81,7 +81,7 @@ def rename_byte_cols(df):
 
 def get_lims_ephys():
 
-    project_codes = ("hIVSCC-MET", "hIVSCC-METx", "hIVSCC-METc", "mIVSCC-MET", "mIVSCC-METx", "qIVSCC-METa", "qIVSCC-METc", "MET-NM", "mMPATCHx", "H301", "H301x")
+    project_codes = ("hIVSCC-MET", "hIVSCC-METx", "hIVSCC-METc", "hIVSCC-MET-SCH", "hIVSCC-METc-SCH", "mIVSCC-MET", "mIVSCC-METx", "mIVSCC-MET-HiMC", "mIVSCC-MET-R01_LC", "mIVSCC-MET-U19_AIBS", "qIVSCC-METa", "qIVSCC-METc", "MET-NM", "mMPATCHx", "H301", "H301x")
     user_codes = ("PC", "PX", "P1", "P2", "P4", "P6", "P8", "P9", "PA", "PB", "PE", "PF", "PI", "PJ", "PN", "PR", "PV")
 
     lims_query = """
