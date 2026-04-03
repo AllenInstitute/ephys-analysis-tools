0608c956c864540d1330eefc1448151f79bc0959
leebrianr <brianle@alleninstitute.org>
2026-04-03 10:31:37 -0700

    Revert "fetal NHP changes"
    This reverts commit 60d3d17e7aa33a7adf490ae4b2011c281c42b7b9.

--- batch_files/ivscc_data_generation/run_generate_user_daily.bat
+++ batch_files/ivscc_data_generation/run_generate_user_daily.bat
@@ -1,5 +1,5 @@
 @rem %USERPROFILE% = C:\Users\%USERNAME%
-call %USERPROFILE%\AppData\Local\anaconda3\Scripts\activate.bat
+call %USERPROFILE%\Anaconda3\Scripts\activate.bat
 call cd..\..
 call activate ephys-analysis-tools-env
 call python src\run_scripts\generate_user_daily.py
